f9bea7fcb11355ac696af0c9d5695441aa5daf64
Vivek Chauhan <48582432+Vivekagent47@users.noreply.github.com>
2020-11-11 17:56:59 +0530

    Some bugs fixes

--- js/style.js
+++ js/style.js
@@ -1,8 +1,8 @@
-var type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
-    radius = '15em', //distance from center
-    start = 0, //shift start from 0
+var type = 1,
+    radius = '15em',
+    start = 0,
     $elements = $('.skills-list>ul>li:not(:first-child)'),
-    numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
+    numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1, 
     slice = 360 * type / numberOfElements;
 
 $elements.each(function(i) {

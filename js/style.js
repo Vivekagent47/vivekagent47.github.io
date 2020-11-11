1ea6d39b5b704619133e6a16d9d5be424c539756
Vivek Chauhan <48582432+Vivekagent47@users.noreply.github.com>
2020-11-11 12:28:59 +0530

    Only for laptop and Pcs

--- js/style.js
+++ js/style.js
@@ -1,3 +1,16 @@
-var app = new Vue({
-    el: "#home"
-})
+var type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
+    radius = '15em', //distance from center
+    start = 0, //shift start from 0
+    $elements = $('.skills-list>ul>li:not(:first-child)'),
+    numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
+    slice = 360 * type / numberOfElements;
+
+$elements.each(function(i) {
+    var $self = $(this),
+        rotate = slice * i + start,
+        rotateReverse = rotate * -1;
+    
+    $self.css({
+        'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
+    });
+});

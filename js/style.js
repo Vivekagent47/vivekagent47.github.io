f8aa6e3f0e9361292bd48a42674944a9a86fa46f
Vivek Chauhan <48582432+Vivekagent47@users.noreply.github.com>
2020-12-03 03:50:48 +0530

    Comments

--- js/style.js
+++ js/style.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($) {
+    // This function is used to arrange the all skills imgs in the circular manner. Where slice defines that in how many parts the cicle is divided and arrange images in that part of the slice. You can define radius by your own.In this case I want to make it fit according to me. But you can cange it dynamically by using responsive widht value.
     var circle = function() {
         var type = 1,
         radius = '15em',
@@ -17,7 +18,8 @@ jQuery(document).ready(function($) {
             });
         }); 
     }
-    
+
+    // This fuction used to remove the inline style of the element that was given by the circle fucntion and add moile classs to arrange them in the way I want to.
     var alterClass = function() {
         var ww = document.body.clientWidth;
         if (ww < 900) {
@@ -30,9 +32,10 @@ jQuery(document).ready(function($) {
             circle();
         };
     };
+
+    // Here I call the function that I define avobe when the width of the page or window (thats mean responsive widht) changes for arranging the items in my own manner.
     $(window).resize(function(){
         alterClass();
-        circle();
     });
     alterClass();
 });

9120f2430da9aa9baef343e629a6fc16d3b73828
Satyam <stym@example.com>
2021-10-06 20:52:15 +0530

    #6 Make Contact Form and Update Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,18 +63,37 @@ tabs.forEach(tab => {
 })
 
 
-let swiperPortfolio = new Swiper(".portfolio__container", {
-	cssMode: true,
-	loop: true,
-	navigation: {
-	  	nextEl: ".swiper-button-next",
-	  	prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-	  	el: ".swiper-pagination",
-		clickable: true,
-	},
-});
+// let swiperPortfolio = new Swiper(".portfolio__container", {
+// 	cssMode: true,
+// 	loop: true,
+// 	navigation: {
+// 	  	nextEl: ".swiper-button-next",
+// 	  	prevEl: ".swiper-button-prev",
+// 	},
+// 	pagination: {
+// 	  	el: ".swiper-pagination",
+// 		clickable: true,
+// 	},
+// });
+
+let swiperPortfolio = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true
+    },
+    autoplay: {
+        delay:1750,
+    },
+   
+  });
+
 
 
 // let swiperTestimonial = new Swiper(".testimonial__container", {

e38600d4c4ac9bc0450ca32aaa664a19d93d4f1c
Vivekagent47 <vivek.agent47@gmail.com>
2022-09-11 14:40:11 +0530

    Update new design

---
+++ components/Seo/Meta.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import { METADATA } from "../../constants";
+
+const Meta = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>{METADATA.title}</title>
+        <meta name="description" content={METADATA.description} />
+        <meta
+          name="keywords"
+          content="Vivek Chauhan, Full Stack Developer, Software Developer, Software Engineer, Portfolio, Web Developer"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Vivek Chauhan" />
+        <meta httpEquiv="content-language" content="en" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={METADATA.title} />
+        <meta property="og:description" content={METADATA.description} />
+
+        <meta property="og:url" content={METADATA.siteUrl} />
+        <meta property="og:site_name" content={METADATA.title} />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={METADATA.title} />
+        <meta property="twitter:description" content={METADATA.description} />
+        <meta property="twitter:site" content={METADATA.twitterHandle} />
+        <meta name="twitter:creator" content={METADATA.twitterHandle} />
+        <meta property="twitter:url" content={METADATA.siteUrl} />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/logo.svg"
+        />
+        <meta name="msapplication-TileColor" content="#7000FF" />
+      </Head>
+      {children}
+    </>
+  );
+};
+
+export default Meta;
---
+++ constants.js
@@ -0,0 +1,112 @@
+export const METADATA = {
+  title: "Portfolio | Vivek Chauhan",
+  description:
+    "Vivek Chauhan is a Full Stack Developer from India, interested in crafting beautiful and functional applications.",
+  siteUrl: "https://vivekagent47.github.io/",
+  twitterHandle: "@itsVivEKagent",
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "home",
+  },
+  {
+    name: "Skills",
+    ref: "skills",
+  },
+  {
+    name: "Projects",
+    ref: "projects",
+  },
+];
+
+export const TYPED_STRINGS = [
+  "A pragmatic Full Stack Developer",
+  "I build things for the web",
+  "I create aesthetic and modern apps",
+];
+
+export const SOCIAL_LINKS = [
+  {
+    name: "mail",
+    url: "mailto: vivekc0679@gmail.com",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/itsvivekchauhan/",
+  },
+  {
+    name: "github",
+    url: "https://github.com/Vivekagent47",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/vivek.agent47/",
+  },
+  {
+    name: "twitter",
+    url: "https://twitter.com/itsVivEKagent",
+  },
+];
+
+export const SKILLS = {
+  languagesAndTools: [
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "python",
+    "figma",
+    "sass",
+  ],
+  librariesAndFrameworks: [
+    "react",
+    "redux",
+    "vuejs",
+    "nodejs",
+    "nextjs",
+    "tailwindcss",
+    "styledcomponents",
+  ],
+  databases: ["mysql", "mongodb"],
+  other: [, "git"],
+};
+
+export const PROJECTS = [
+  {
+    name: "Typing Speed",
+    image: "/projects/speedType.webp",
+    description: "Check Your Typing Speed",
+    gradient: ["#F14658", "#DC2537"],
+    url: "https://vivekagent47.github.io/typingSpeed/",
+    tech: ["javascript", "html", "css"],
+  },
+  {
+    name: "Coviz",
+    image: "/projects/coviz.webp",
+    description:
+      "Track current and historical covid-19 data of India and its states and union territories. Reactjs + Material UI",
+    gradient: ["#FFA62E", "#EA4D2C"],
+    url: "https://vivekagent47coviz.netlify.app/",
+    tech: ["react", "javascript", "figma"],
+  },
+  {
+    name: "Sorting Performance",
+    image: "/projects/sorting.webp",
+    description: "Compare performance of the various sorting algorithms.",
+    gradient: ["#000066", "#6699FF"],
+    url: "https://github.com/anuragdvd/SortingPerformance",
+    tech: [],
+  },
+  {
+    name: "Demo Portfolio",
+    image: "/projects/portfolio.webp",
+    description: "Just Portfolio Template",
+    gradient: ["#142D46", "#2E4964"],
+    url: "",
+    tech: ["html", "css", "javascript"],
+  },
+];
+
+export const GTAG = "G-5HCTL2TJ5W";
